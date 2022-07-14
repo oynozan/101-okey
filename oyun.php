33772aaef287edf0f826d604731c609b97b8f231
Ozan <47722606+oynozan@users.noreply.github.com>
2022-07-14 23:47:26 +0300

    Update oyun.php

--- oyun.php
+++ oyun.php
@@ -7,8 +7,8 @@
     <title>101 Okey</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/main.css?v=2">
-    <link rel="stylesheet" href="css/oyun.css?v=2">
+    <link rel="stylesheet" href="css/main.css?v=3">
+    <link rel="stylesheet" href="css/oyun.css?v=3">
 </head>
 <body>
     <div class="container-fluid max-width-1920">
